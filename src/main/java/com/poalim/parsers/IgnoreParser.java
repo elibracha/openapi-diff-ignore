99a172bb87541359e410b447be70faf9e89be24f
eli <elibracha45@gmail.com>
2020-02-04 00:24:36 +0200

    adding logging dependencies

--- src/main/java/com/poalim/parsers/IgnoreParser.java
+++ src/main/java/com/poalim/parsers/IgnoreParser.java
@@ -1,4 +1,10 @@
 package com.poalim.parsers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class IgnoreParser {
+
+    static final Logger logger = LoggerFactory.getLogger(IgnoreParser.class);
+
 }
