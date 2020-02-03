99b6c886b37bb0abb71dde0601839a3b67d29e3a
eli <elibracha45@gmail.com>
2020-02-04 01:25:44 +0200

    testing code cov

---
+++ src/test/java/com/poalim/parsers/IgnoreParserTest.java
@@ -0,0 +1,13 @@
+package com.poalim.parsers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IgnoreParserTest {
+
+    @Test
+    public void testNameMkyong() {
+        assertEquals("Hello", "Hello");
+    }
+}
