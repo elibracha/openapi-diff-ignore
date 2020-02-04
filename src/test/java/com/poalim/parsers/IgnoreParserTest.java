efacacb0dfccf96a110b3208ed4b9bb23205ceeb
Kfir <kfirfer@gmail.com>
2020-02-04 16:32:09 +0200

    Adding general library test

--- src/test/java/com/poalim/parsers/IgnoreParserTest.java
+++ src/test/java/com/poalim/parsers/IgnoreParserTest.java
@@ -1,5 +1,15 @@
 package com.poalim.parsers;
 
+import com.poalim.parsers.processors.IgnoreProcessor;
+import org.junit.Test;
+
 public class IgnoreParserTest {
 
+
+    @Test
+    public void test() {
+        IgnoreProcessor parser = new IgnoreProcessor(".ignore.yml");
+        parser.processIgnore();
+        System.out.println(parser.getMapKey());
+    }
 }
