6f74653b48916ce4299c271d16dfdeb1e06aec57
eli <elibracha45@gmail.com>
2020-02-12 18:31:35 +0200

    SchemaDeserializer ignore all supported

--- src/main/java/org/openapi/diff/ignore/deserializes/ContentSchemaDeserializer.java
+++ src/main/java/org/openapi/diff/ignore/deserializes/ContentSchemaDeserializer.java
@@ -1,4 +1,49 @@
 package org.openapi.diff.ignore.deserializes;
 
-public class ContentSchemaDeserializer {
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import org.openapi.diff.ignore.ObjectMapperFactory;
+import org.openapi.diff.ignore.exceptions.SpecificationSupportException;
+import org.openapi.diff.ignore.models.ignore.ContentProperties;
+import org.openapi.diff.ignore.models.ignore.ContentSchema;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+public class ContentSchemaDeserializer extends StdDeserializer<ContentSchema> {
+
+    public ContentSchemaDeserializer() {
+        super(ContentSchema.class);
+    }
+
+    @Override
+    public ContentSchema deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        JsonNode contentSchemaScope = jsonParser.getCodec().readTree(jsonParser);
+        ContentSchema contentSchema = new ContentSchema();
+
+        if (!contentSchemaScope.isContainerNode()) {
+            contentSchema.setIgnoreAll(true);
+            return contentSchema;
+        }
+
+        for (Iterator<Map.Entry<String, JsonNode>> it = contentSchemaScope.fields(); it.hasNext(); ) {
+            Map.Entry<String, JsonNode> contentPropertiesScope = it.next();
+
+            switch (contentPropertiesScope.getKey()) {
+                case "schema":
+                    ContentProperties contentIgnore = ObjectMapperFactory.createYaml().convertValue(contentPropertiesScope.getValue(), ContentProperties.class);
+                    contentSchema.setSchema(contentIgnore);
+                    break;
+                default:
+                    throw new SpecificationSupportException(String.format(
+                            "Specification does not support value \"%s\" as an schema ignore type, please referenced the documentation for supported entries.",
+                            contentPropertiesScope.getKey()));
+            }
+        }
+
+        return contentSchema;
+    }
 }
--- src/main/java/org/openapi/diff/ignore/models/ignore/ContentSchema.java
+++ src/main/java/org/openapi/diff/ignore/models/ignore/ContentSchema.java
@@ -1,13 +1,16 @@
 package org.openapi.diff.ignore.models.ignore;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.openapi.diff.ignore.deserializes.ContentSchemaDeserializer;
 import org.openapi.diff.ignore.models.IgnoreElemnt;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@JsonDeserialize(using = ContentSchemaDeserializer.class)
 public class ContentSchema extends IgnoreElemnt {
     private ContentProperties schema;
 }
