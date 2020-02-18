d42621d961231c424e0eedc4b41ba32d7ae5e93f
eli <elibracha45@gmail.com>
2020-02-18 11:49:57 +0200

    adding response test

--- src/test/java/org/openapi/diff/ignore/DeserializersTest.java
+++ src/test/java/org/openapi/diff/ignore/DeserializersTest.java
@@ -12,21 +12,21 @@ public class DeserializersTest {
     @Test
     public void testContextDeserialization() throws FileNotFoundException {
         TestUtil util = new TestUtil();
-        ContextIgnore contextIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap(".context"), ContextIgnore.class);
+        ContextIgnore contextIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap("deserialize/.context_deserialize"), ContextIgnore.class);
         assertEquals(util.getContext(), contextIgnoreFromFile);
     }
 
     @Test
     public void testPathsDeserialization() throws FileNotFoundException {
         TestUtil util = new TestUtil();
-        PathsIgnore contextIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap(".paths"), PathsIgnore.class);
+        PathsIgnore contextIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap("deserialize/.paths_deserialize"), PathsIgnore.class);
         assertEquals(util.getPaths(), contextIgnoreFromFile);
     }
 
     @Test
     public void testSecurityDeserialization() throws FileNotFoundException {
         TestUtil util = new TestUtil();
-        SecurityIgnore securityIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap(".security"), SecurityIgnore.class);
+        SecurityIgnore securityIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap("deserialize/.security_deserialize"), SecurityIgnore.class);
         assertEquals(util.getSecurityPost(), securityIgnoreFromFile);
 
     }
@@ -34,7 +34,7 @@ public class DeserializersTest {
     @Test
     public void testContentDeserialization() throws FileNotFoundException {
         TestUtil util = new TestUtil();
-        Content contentIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap(".content"), Content.class);
+        Content contentIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap("deserialize/.content_deserialize"), Content.class);
         assertEquals(util.getContentPost(), contentIgnoreFromFile);
 
     }
@@ -42,28 +42,28 @@ public class DeserializersTest {
     @Test
     public void testRequestDeserialization() throws FileNotFoundException {
         TestUtil util = new TestUtil();
-        RequestIgnore requestIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap(".request"), RequestIgnore.class);
+        RequestIgnore requestIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap("deserialize/.request_deserialize"), RequestIgnore.class);
         assertEquals(util.getRequestPost(), requestIgnoreFromFile);
     }
 
     @Test
     public void testResponseDeserialization() throws FileNotFoundException {
         TestUtil util = new TestUtil();
-        ResponseIgnore responseIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap(".response"), ResponseIgnore.class);
+        ResponseIgnore responseIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap("deserialize/.response_deserialize"), ResponseIgnore.class);
         assertEquals(util.getResponsePost(), responseIgnoreFromFile);
     }
 
     @Test
     public void testOperationDeserialization() throws FileNotFoundException {
         TestUtil util = new TestUtil();
-        OperationIgnore operationIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap(".operation"), OperationIgnore.class);
+        OperationIgnore operationIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap("deserialize/.operation_deserialize"), OperationIgnore.class);
         assertEquals(util.getOperationPost(), operationIgnoreFromFile);
     }
 
     @Test
     public void testHttpDeserialization() throws FileNotFoundException {
         TestUtil util = new TestUtil();
-        HttpMethodIgnore httpMethodIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap(".http_method"), HttpMethodIgnore.class);
+        HttpMethodIgnore httpMethodIgnoreFromFile = ObjectMapperFactory.createYaml().convertValue(util.loadMap("deserialize/.http_method_deserialize"), HttpMethodIgnore.class);
         assertEquals(util.getHttpMethodPost(), httpMethodIgnoreFromFile);
     }
 }
--- src/test/java/org/openapi/diff/ignore/ValidatorsTest.java
+++ src/test/java/org/openapi/diff/ignore/ValidatorsTest.java
@@ -1,4 +1,36 @@
 package org.openapi.diff.ignore;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.openapi.diff.ignore.validators.ResponseValidator;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class ValidatorsTest {
+
+    @Test
+    public void testResponseValidationTrue() throws IOException {
+        ResponseValidator responseValidator = new ResponseValidator();
+        ObjectMapper objectMapper = ObjectMapperFactory.createYaml();
+        byte[] content = Files.readAllBytes(Paths.get(getClass().getResource("/validate/.response_validate_true").getPath()));
+
+        responseValidator.setResponse(objectMapper.readTree(new String(content, StandardCharsets.UTF_8)));
+        assertTrue(responseValidator.validate());
+    }
+
+    @Test
+    public void testResponseValidationFalse() throws IOException {
+        ResponseValidator responseValidator = new ResponseValidator();
+        ObjectMapper objectMapper = ObjectMapperFactory.createYaml();
+        byte[] content = Files.readAllBytes(Paths.get(getClass().getResource("/validate/.response_validate_false").getPath()));
+
+        responseValidator.setResponse(objectMapper.readTree(new String(content, StandardCharsets.UTF_8)));
+        assertFalse(responseValidator.validate());
+    }
 }
