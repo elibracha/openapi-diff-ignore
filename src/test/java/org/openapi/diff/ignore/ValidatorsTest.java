6b4d2253a2dbbf73eb725bd21e4aa085e4135450
Charcon <sharkonz@gmail.com>
2020-02-25 14:36:07 +0200

    fixed relative path.

--- src/test/java/org/openapi/diff/ignore/ValidatorsTest.java
+++ src/test/java/org/openapi/diff/ignore/ValidatorsTest.java
@@ -5,9 +5,6 @@ import org.junit.Test;
 import org.openapi.diff.ignore.validators.*;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertFalse;
