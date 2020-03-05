4d5041bea6576fd252e2ac2d6fd0dc72d24179d4
Charcon <sharkonz@gmail.com>
2020-03-05 18:30:26 +0200

    wip

--- src/test/java/org/openapi/diff/ignore/ChangeTest.java
+++ src/test/java/org/openapi/diff/ignore/ChangeTest.java
@@ -240,54 +240,6 @@ public class ChangeTest {
 
         assertTrue(changedOpenApi.isUnchanged());
     }
-    @Test
-    public void testResponseIncreasedTrue() throws SpecificationSupportException {
-        ContextProcessor contextProcessor = new ContextProcessor(
-                getClass().getClassLoader().getResource("changes/response/response_increased_true/diffignore.yaml").getFile()
-        );
-
-        ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations("changes/response/response_increased_true/original.yaml", "changes/response/response_increased_true/generated.yaml");
-
-        ChangedOpenApi changedOpenApiAfter = contextProcessor.process(changedOpenApi);
-
-        String html =
-                new HtmlRender("Changelog", "http://deepoove.com/swagger-diff/stylesheets/demo.css")
-                        .render(changedOpenApiAfter);
-        try {
-            FileWriter fw = new FileWriter("target/ResponseIncreasedTrue.html");
-            fw.write(html);
-            fw.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        assertTrue(changedOpenApi.isUnchanged());
-    }
-    @Test
-    public void testResponseIncreasedFalse() throws SpecificationSupportException {
-        ContextProcessor contextProcessor = new ContextProcessor(
-                getClass().getClassLoader().getResource("changes/response/response_increased_false/diffignore.yaml").getFile()
-        );
-
-        ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations("changes/response/response_increased_false/original.yaml", "changes/response/response_increased_false/generated.yaml");
-
-        ChangedOpenApi changedOpenApiAfter = contextProcessor.process(changedOpenApi);
-
-        String html =
-                new HtmlRender("Changelog", "http://deepoove.com/swagger-diff/stylesheets/demo.css")
-                        .render(changedOpenApiAfter);
-        try {
-            FileWriter fw = new FileWriter("target/ResponseIncreasedFalse.html");
-            fw.write(html);
-            fw.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        assertFalse(changedOpenApi.isUnchanged());
-    }
 
     @Test
     public void testResponseIncreasedTrue() throws SpecificationSupportException {
