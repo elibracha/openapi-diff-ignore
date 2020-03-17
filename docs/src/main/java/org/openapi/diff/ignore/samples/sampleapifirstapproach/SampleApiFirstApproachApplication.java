3ff274098e347f209348648ac8955f1c0186894b
eli bracha <elibracha45@gmail.com>
2020-03-17 11:50:32 +0200

    structure with openapi-generator

---
+++ docs/src/main/java/org/openapi/diff/ignore/samples/sampleapifirstapproach/SampleApiFirstApproachApplication.java
@@ -0,0 +1,13 @@
+package org.openapi.diff.ignore.samples.sampleapifirstapproach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApiFirstApproachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApiFirstApproachApplication.class, args);
+    }
+
+}
---
+++ docs/src/test/java/org/openapi/diff/ignore/samples/sampleapifirstapproach/SampleApiFirstApproachApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openapi.diff.ignore.samples.sampleapifirstapproach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApiFirstApproachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
